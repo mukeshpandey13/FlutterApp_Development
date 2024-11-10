e9a9c16836e1eaa96cb3b081375c42ca1e144dd5
Pande Mukesh <pandeymukesh13>
2024-11-10 18:15:26 +0545

    #6 - Colours & Fonts

--- lib/main.dart
+++ lib/main.dart
@@ -5,17 +5,47 @@ void main() => runApp(MaterialApp(
     appBar: AppBar(
       title: Text('MyApp'),
       centerTitle: true,
+
+// For background color
+      backgroundColor: Colors.red[600],
     ),
     body: Center(
-      child: Text('Hello, World!'), // Example content
+      child: Text(
+        'Hello world',
+
+        // style 
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.grey[600],
+          fontFamily: 'IndieFlower',
+        ),
+      ), 
     ),
+
     // for button
     floatingActionButton: FloatingActionButton(
-      onPressed: () {
-        // Add your onPressed code here!
-        print('Button clicked!');
-      },
-      child: Icon(Icons.add), // Use an Icon instead of Text
+      onPressed: () {},
+      child: Text('click'),
+
+// button color
+      backgroundColor: Colors.red[600],
     ),
   ),
 ));
+
+
+// NOte (this course lesson)
+// --Background color --color
+// -- button
+//    -- color
+
+// -- button color (line 20)
+// -- text style (line 16-21)
+//      -- change of text style
+
+// -- change font style to IndieFlower
+//    -- go to 'pubspec.yaml' file
+//    -- line no 77 
+//    -- change font using asset
