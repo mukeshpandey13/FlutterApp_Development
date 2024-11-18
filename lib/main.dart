30b4424768f2057a885bc1badf9cd49b780ccc2d
Pande Mukesh <pandeymukesh13>
2024-11-18 16:49:05 +0545

    #9 - Buttons & Icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,68 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home:Home(), 
-));
+      home: Home(),
+    ));
 
-
-// stateless widget
+// Stateless widget
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    appBar: AppBar(
-      title: Text('MyApp'),
-      centerTitle: true,
+      appBar: AppBar(
+        title: Text('MyApp'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+          // Button with onPressed callback
+          //##Elevated button (just uncomment to use elevated button)
+          // child: ElevatedButton(
 
-// For background color
-      backgroundColor: Colors.red[600],
-    ),
-    body: Center(
+          // ## text button
+          // child: TextButton(
+          //   onPressed: () {
+          //     print('You clicked me');
+          //   },
 
-      // for image 
-      // ## LOCAL IMAGE
-      child: Image.asset('assetss/image2.jpg'),
+          //   child: Text('Click me'),
+          //   style: ElevatedButton.styleFrom(
+          //     backgroundColor: Colors.black12
+          //   ),
+          // ),
 
+          // ##adding icon in button
+        //  child: ElevatedButton.icon(onPressed: (){},
+        //  icon: Icon(Icons.mail),
+        //   label: Text('Mail Me'),
+        //   style: ElevatedButton.styleFrom(
+        //     backgroundColor: Colors.cyan
+        //   ),
+        //   ),
 
-      // ## NETWORK IMAGE
-      // child: Image(image: NetworkImage('https://images.pexels.com/photos/29276458/pexels-photo-29276458/free-photo-of-colorful-wooden-houses-in-bergen-norway.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'))
-    ),
-
-    // for button
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {},
-      child: Text('click me'),
-
-// button color
-      backgroundColor: Colors.red[600],
-    ),
-  );
+        //## clickable icon
+        child: IconButton(onPressed: (){
+          print('You click Me');
+        }, icon: Icon(Icons.home),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.amberAccent
+        ),
+        ),
+          ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('Click'),
+        backgroundColor: Colors.red[600],
+      ),
+    );
   }
 }
+
 // NOte (this course lesson)
-// --1)Network Image (line2 5)
-//     child:Image.Network('url'),
-// --2)Assets Image(line 26)
-//     child.Image.asset('path'),
+// --1) Icon (linet 25)
+// --2) ElevatedButton (linet 22)
+// --3)Text  buttton (linet 25)
+// ##adding icon in button (linet 36)
+//## clickable icon(line 45  to52)
